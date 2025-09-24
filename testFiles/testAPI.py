c41c1deefe6e1f3174043d11282340fab357c7c7
Saiful I <saifulis.4965@gmail.com>
2025-09-25 00:40:28 +0530

    remove unused client initialization in testAPI.py

--- testFiles/testAPI.py
+++ testFiles/testAPI.py
@@ -1,7 +1,6 @@
 from google import genai
 import os
 
-client = genai.Client(api_key="AIzaSyC_Qry3JDi01etv7ncMxQa8dLG480w-a_8")
 
 resp = client.models.generate_content(
     model="gemini-2.5-flash",
