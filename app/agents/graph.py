9d2161d1c07fedbff3b8eb141940ce8090a223b2
Saiful I <saifulis.4965@gmail.com>
2025-09-30 16:43:04 +0530

    Langraph update

--- app/agents/graph.py
+++ app/agents/graph.py
@@ -5,6 +5,7 @@ from app.agents.coinNameToSymbolAgent import getCoinSymbol
 from app.agents.financeMetricAgent import get_finance_metrics
 from app.agents.platformSelectionAgent import get_platform_selection
 from app.agents.final_advice_agent import getFinalAdvice
+from app.agents.finalReturnAmountAgent import estimate_crypto_return_with_csv
 
 
 from app.schemas.schemas import FinanceMetrics, ExchangeComparison, UserRequest, CryptoAdvice
@@ -14,6 +15,7 @@ class GraphState(TypedDict):
     input: UserRequest
     coinSymbol: str
     finance_metrics: dict
+    estimated_return_usd: dict
     platform_selection: str
     final_advice: dict
 
@@ -23,16 +25,19 @@ def build_graph():
     finance_metrics_fn = get_finance_metrics()
     platform_selection_fn = get_platform_selection()
     final_advice_fn = getFinalAdvice()
+    estimate_return_fn = estimate_crypto_return_with_csv()
 
     graph = StateGraph(GraphState)
     graph.add_node("coinSymbol", coin_symbol_fn)
     graph.add_node("finance_metrics", finance_metrics_fn)
     graph.add_node("platform_selection", platform_selection_fn)
     graph.add_node("final_advice", final_advice_fn)
+    graph.add_node("estimated_return_usd", estimate_return_fn)
 
     graph.set_entry_point("coinSymbol")
     graph.add_edge("coinSymbol", "finance_metrics")
-    graph.add_edge("finance_metrics", "platform_selection")
+    graph.add_edge("finance_metrics", "estimated_return_usd")
+    graph.add_edge("estimated_return_usd", "platform_selection")
     graph.add_edge("platform_selection", "final_advice")
     graph.set_finish_point("final_advice")
 
