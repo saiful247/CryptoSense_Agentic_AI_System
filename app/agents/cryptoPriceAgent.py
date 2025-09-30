4eaa1c666a0f790a832e4f9dd2a2c8bb217e603c
Saiful I <saifulis.4965@gmail.com>
2025-09-30 14:33:12 +0530

    refactor cryptoPriceAgent to improve instruction clarity and enhance error handling

--- app/agents/cryptoPriceAgent.py
+++ app/agents/cryptoPriceAgent.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pydantic import BaseModel
 
+
 class CryptoPrice(BaseModel):
     current_price: float
     percent_change_1h: float
@@ -17,9 +18,11 @@ class CryptoPrice(BaseModel):
     market_cap: float
     volume_24h: float
 
+
 class CryptoPriceAgentResponse(BaseModel):
     summary: str
     all_price: CryptoPrice
+
 
 load_dotenv()
 
@@ -39,6 +42,7 @@ headers = {
 }
 
 # def getCryptoPrice(prompt:str)->str:
+
 
 def cryptoPriceTool(coinSymbol: str) -> str:
     """This agent is used to get the price info about a specific coin"""
@@ -68,117 +72,118 @@ def cryptoPriceTool(coinSymbol: str) -> str:
     return json.dumps(coin_usd)
 
 
-instruction = """
-You are a crypto price agent. You have acces to Price fetching tool.
-steps to follow:
-1. first you need to convert the user query into a coin symbol. for example if user query is "give me price of bitcoin" then the coin symbol is "BTC"
-2. then you need to call the cryptoPriceTool with the coin symbol to get the price.
-3. then cryptoPriceTool will return a JSON string with the price data.
-4. then you need to parse the JSON string and extract the following fields:
-- current_price
-- percent_change_1h
-- percent_change_24h
-- percent_change_7d
-- percent_change_30d
-5. the you need to give the price to the user like below JSON format:
-    {{
-        summary: "The current price of Bitcoin is $4342.08, with a 1-hour change of 0.14%, a 24-hour change of -3.41%, and a 7-day change of -9.47%. The market cap is $524119084067.21 and the volume in the last 24 hours is $46773360124.46.",
-        all_price: {{
-            "current_price": 4342.08,
-            "percent_change_1h": 0.14,
-            "percent_change_24h": -3.41,
-            "percent_change_7d": -9.47,
-            "percent_change_30d": 13.83,
-            "market_cap": 524119084067.21,
-            "volume_24h": 46773360124.46
+def getCoinPriceData(userPrompt: str) -> dict:
+    instruction = """
+    You are a crypto price agent. You have acces to Price fetching tool.
+    steps to follow:
+    1. first you need to convert the user query into a coin symbol. for example if user query is "give me price of bitcoin" then the coin symbol is "BTC"
+    2. then you need to call the cryptoPriceTool with the coin symbol to get the price.
+    3. then cryptoPriceTool will return a JSON string with the price data.
+    4. then you need to parse the JSON string and extract the following fields:
+    - current_price
+    - percent_change_1h
+    - percent_change_24h
+    - percent_change_7d
+    - percent_change_30d
+    5. the you need to give the price to the user like below JSON format:
+        {{
+            summary: "The current price of Bitcoin is $4342.08, with a 1-hour change of 0.14%, a 24-hour change of -3.41%, and a 7-day change of -9.47%. The market cap is $524119084067.21 and the volume in the last 24 hours is $46773360124.46.",
+            all_price: {{
+                "current_price": 4342.08,
+                "percent_change_1h": 0.14,
+                "percent_change_24h": -3.41,
+                "percent_change_7d": -9.47,
+                "percent_change_30d": 13.83,
+                "market_cap": 524119084067.21,
+                "volume_24h": 46773360124.46
+            }}
         }}
-    }}
-6.After the JSON, output a single line: TERMINATE
-"""
+    6.After the JSON, output a single line: TERMINATE
+    """
 
-cryptoPriceAgent = ConversableAgent(
-    "cryptoPriceAgent",
-    system_message=instruction,
-    llm_config={
-        'config_list': config_list
-    },
-    max_consecutive_auto_reply=2,
-    # This setting tells the agent how many back-to-back replies it’s allowed 
-    # to generate automatically
-    # (without human input) before the framework forces the conversation to stop.
-    # Prevents infinite loops where two agents keep talking endlessly.
-    # if is_termination_msg met within the autoreply it stops.
-    human_input_mode="NEVER",
+    cryptoPriceAgent = ConversableAgent(
+        "cryptoPriceAgent",
+        system_message=instruction,
+        llm_config={
+            'config_list': config_list
+        },
+        max_consecutive_auto_reply=2,
+        # This setting tells the agent how many back-to-back replies it’s allowed
+        # to generate automatically
+        # (without human input) before the framework forces the conversation to stop.
+        # Prevents infinite loops where two agents keep talking endlessly.
+        # if is_termination_msg met within the autoreply it stops.
+        human_input_mode="NEVER",
 
-)
+    )
 
-user_proxy = ConversableAgent(
-    "user_proxy",
-    llm_config=False,
-    human_input_mode="NEVER",
-    is_termination_msg=lambda msg: msg.get(
-        "content") is not None and "TERMINATE" in msg["content"],  #if the message(reply) from CryptoPriceAgent contains TERMINATE, then stop
-)
+    user_proxy = ConversableAgent(
+        "user_proxy",
+        llm_config=False,
+        human_input_mode="NEVER",
+        is_termination_msg=lambda msg: msg.get(
+            # if the message(reply) from CryptoPriceAgent contains TERMINATE, then stop
+            "content") is not None and "TERMINATE" in msg["content"],
+    )
 
-register_function(
-    f=cryptoPriceTool,
-    caller=cryptoPriceAgent,
-    executor=user_proxy, #this agent is responsible for actually running the Python code.
-    description="Fetches the latest price and market data for a specified cryptocurrency symbol (e.g., BTC for Bitcoin, ETH for Ethereum)."
-)
+    register_function(
+        f=cryptoPriceTool,
+        caller=cryptoPriceAgent,
+        # this agent is responsible for actually running the Python code.
+        executor=user_proxy,
+        description="Fetches the latest price and market data for a specified cryptocurrency symbol (e.g., BTC for Bitcoin, ETH for Ethereum)."
+    )
 
-"""
-when register function, the function signature is automatically converted to JSON schema like below:
-so that the LLM can understand what parameters to pass.
-so thats why the cryptoPriceAgent responss to user_proxy as. this is why the respoonse exactly
-conatining the same parameter name.
-Arguments:
-{"coinSymbol": "BTC"}
+    """
+    when register function, the function signature is automatically converted to JSON schema like below:
+    so that the LLM can understand what parameters to pass.
+    so thats why the cryptoPriceAgent responss to user_proxy as. this is why the respoonse exactly
+    conatining the same parameter name.
+    Arguments:
+    {"coinSymbol": "BTC"}
 
-{
-"name": "cryptoPriceTool",
-"description": "Fetches the latest price and market data for a specified cryptocurrency symbol (e.g., BTC for Bitcoin, ETH for Ethereum).",
-"parameters": {
-    "type": "object",
-    "properties": {
-    "coinSymbol": {
-        "type": "string",
-        "description": ""
+    {
+    "name": "cryptoPriceTool",
+    "description": "Fetches the latest price and market data for a specified cryptocurrency symbol (e.g., BTC for Bitcoin, ETH for Ethereum).",
+    "parameters": {
+        "type": "object",
+        "properties": {
+        "coinSymbol": {
+            "type": "string",
+            "description": ""
+        }
+        },
+        "required": ["coinSymbol"]
     }
-    },
-    "required": ["coinSymbol"]
-}
-}
+    }
 
-"""
+    """
 
-prompt="I need the price of Solana"
+    prompt = userPrompt
 
-result = user_proxy.initiate_chat(
-    cryptoPriceAgent, message=prompt)
+    result = user_proxy.initiate_chat(
+        cryptoPriceAgent, message=prompt)
 
+    final_text = result.chat_history[3].get("content", "")
+    print("Final Result 007: ", final_text)
 
-final_text= result.chat_history[3].get("content","")
-print("Final Result 007: ", final_text)
-
-try:
-    final_text = re.sub(r"^```json\s*","", final_text)
-    final_text = re.sub(r"\s*```\s*","", final_text)
-    final_text = re.sub(r"\s*TERMINATE\s*$","", final_text)
-    print("After cleaning: ", final_text)
-    final_result = json.loads(final_text)
-    # final_result=final_text
-    try: 
-        print("Final Result XX str: ", final_result)
-        # final_output=CryptoPriceAgentResponse(**final_result)
-        # print("Pydantic Schema successfully parsed: ", final_output)
-        # return final_output
-        # return final_result
+    try:
+        final_text = re.sub(r"^```json\s*", "", final_text)
+        final_text = re.sub(r"\s*```\s*", "", final_text)
+        final_text = re.sub(r"\s*TERMINATE\s*$", "", final_text)
+        print("After cleaning: ", final_text)
+        # final_result = json.loads(final_text)
+        final_result = final_text
+        try:
+            print("Final Result XX str: ", final_result)
+            # final_output=CryptoPriceAgentResponse(**final_result)
+            # print("Pydantic Schema successfully parsed: ", final_output)
+            # return final_output
+            return final_result
+        except Exception as e:
+            print("Error in parsing to pydantic model: ", str(e))
+            return {"Error": str(e)}
     except Exception as e:
-        print("Error in parsing to pydantic model: ", str(e))
-        # return {"Error": str(e)}
-except Exception as e:
-    final_result = {"raw": final_text}
-    print("Error in processing final result: ", str(e))
-    # return {"Error": str(e)}
-
+        final_result = {"raw": final_text}
+        print("Error in processing final result: ", str(e))
+        return {"Error": str(e)}
